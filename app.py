b3a8ef9a4f03587d13d6bef7ea4aabff4b3a80e8
Chungxhung <loveiesschung98@gmail.com>
2023-10-25 18:28:04 +0900

    그래프를 그리기 위한 데이전처리 코드 추가

--- app.py
+++ app.py
@@ -19,18 +19,28 @@ def stock_search():
 
 @app.route('/search', methods=['POST'])
 def search_stock():
+    # isin코드 불러오기
     isinCd = request.form['isinCd']
+    # start date 불러오기
     start_date_str = request.form['start_date']
     start_date_format = datetime.strptime(start_date_str, '%Y-%m-%d')
     start_date = datetime.strftime(start_date_format, '%Y%m%d')
+    # end date 불러오기
     end_date_str = request.form['end_date']
     end_date_format = datetime.strptime(end_date_str, '%Y-%m-%d')
     end_date = datetime.strftime(end_date_format, '%Y%m%d')
+    # query
     query = {'isinCd' : isinCd,
             'basDt' : {'$gte' : start_date, '$lte' : end_date}
             }
-    result = collection.find(query).sort('basDt', -1)
-    return render_template('stock_results.html', data = result, isinCd = isinCd, start_date = start_date, end_date = end_date)
+    # 결과
+    result1 = collection.find(query).sort('basDt', -1)
+    result2 = list(collection.find(query, {'basDt' : 1, 'clpr': 1, '_id' : 0}).sort('basDt', 1))
+    
+    # 데이터 추출 후 리스트로 변환
+    dates = [item['basDt'] for item in result2]
+    values = [item['clpr'] for item in result2]
+    return render_template('stock_results.html', data = result1, isinCd = isinCd, start_date = start_date, end_date = end_date, dates = dates, values = values)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug = True)
